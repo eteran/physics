3704428abfb586825bd35146e6409ddfe3da3bf9
Evan Sultanik <evan@sultanik.com>
2009-08-18 20:56:06 +0000

    Added.

---
+++ src/com/sultanik/ui/UserInterface.java
@@ -0,0 +1,12 @@
+package com.sultanik.ui;
+
+import java.awt.event.*;
+
+public interface UserInterface {
+    public void setFocusProvider(FocusProvider focusProvider);
+    public void addKeyListener(KeyListener listener);
+    public void removeKeyListener(KeyListener listener);
+    public void addRepaintListener(RepaintListener listener);
+    public void removeRepaintListener(RepaintListener listner);
+    public void repaint();
+}
